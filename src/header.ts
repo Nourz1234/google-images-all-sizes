643dde477727a4607ef3e57da6533f13053fb9af
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2022-09-21 16:59:41 +0200

    Added LICENSE and some info

--- src/header.ts
+++ src/header.ts
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Google Images All Sizes
-// @description  Adds 'All Sizes' and 'Full Size' buttons to google images.
 // @version      1.8.0
+// @description  Adds 'All Sizes' and 'Full Size' buttons to google images.
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @license      MIT
 // @author       Nour Nasser
 // @namespace    https://github.com/Nourz1234
 // @match        *://www.google.com/search*tbm=isch*
@@ -10,5 +11,6 @@
 // @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/Nourz1234/google-images-all-sizes/master/dist/google-images-all-sizes.js
 // @downloadURL  https://raw.githubusercontent.com/Nourz1234/google-images-all-sizes/master/dist/google-images-all-sizes.js
+// @supportURL   https://github.com/Nourz1234/google-images-all-sizes/issues
 // @grant        none
 // ==/UserScript==
